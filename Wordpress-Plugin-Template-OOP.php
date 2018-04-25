bb00e9573411fec9aa914c80da830bcb02a687c1
Kamran Syed <lovelykamran@gmail.com>
2018-04-25 13:25:53 +0500

    Create Wordpress-Plugin-Template-OOP.php

---
+++ Wordpress-Plugin-Template-OOP.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Wordpress Plugin Template
+Plugin URI: https://github.com/KamranSyed/Wordpress-Plugin-Template-OOP
+Description: It is a ready made template for Wordpress and Woocommerce to give you a head start.
+Version: 1.0
+Author: Kamran Syed
+Author URI: http://agilesolutionspk.com/author/kamran-syed/
+*/
+
